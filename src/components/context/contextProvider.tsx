a52edbc53ef3df5386769d1c7375ba5e5409d708
Habib-Gulomov <gulomovhabibulloh@gmail.com>
2024-07-27 14:58:45 +0500

    useState error fixed in provider

--- src/components/context/contextProvider.tsx
+++ src/components/context/contextProvider.tsx
@@ -1,20 +1,18 @@
-import React, {  useState, ReactNode } from 'react';
+import React, { useState, ReactNode } from "react";
 import MyContext from "./context";
 
 interface MyProviderProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
-const [value, setValue] = useState<any>([]);
+const MyProvider: React.FC<MyProviderProps> = ({ children }) => {
+	const [value, setValue] = useState<any>([]);
 
-const MyProvider: React.FC   <MyProviderProps> = ({ children }) => {
-  
-  
-  return (
-    <MyContext.Provider value={{ value, setValue}  }>
-      {children}
-    </MyContext.Provider>
-  );
+	return (
+		<MyContext.Provider value={{ value, setValue }}>
+			{children}
+		</MyContext.Provider>
+	);
 };
 
 export default MyProvider;
